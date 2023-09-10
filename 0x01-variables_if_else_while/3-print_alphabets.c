3562e6d06e6f0abe1e4c6e15187611907aa2f90c
madomah2035 <kwamemusah51@gmail.com>
2023-09-10 16:02:41 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i = 0;
+
+	do {
+		putchar(alphabet[i]);
+		i++;
+	} while (alphabet[i] != '\0');
+
+	putchar('\n');
+
+	return (0);
+}
+
