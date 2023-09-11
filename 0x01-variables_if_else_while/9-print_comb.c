ecf20a99552037b5db27593dbbfd32f82f929d94
madomah2035 <kwamemusah51@gmail.com>
2023-09-10 18:22:14 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
     int num;
@@ -17,5 +22,6 @@ int main(void)
 
     putchar('\n');
 
-    return 0;
+    return (0);
 }
+
