0e2833c0d0b3794be1e3aa1f87be68a338a93ed6
madomah2035 <kwamemusah51@gmail.com>
2023-09-10 17:12:22 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alpha[] = "zyxwvutsrqponmlkjihgfedcba";
+	int i = 0;
+	do {
+		putchar(alpha[i]);
+		i++;
+	} while (alpha[i] != '\0');
+
+	putchar('\n');
+
+	return (0);
+}
