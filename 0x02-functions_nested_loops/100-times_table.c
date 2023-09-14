7464545f038300b6d22587cd1138f1b55836bd7b
madomah2035 <kwamemusah51@gmail.com>
2023-09-13 22:04:06 -0700

    times_table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,47 +6,47 @@
  */
 void print_times_table(int n)
 {
-    int i, j, result;
+	int i, j, result;
 
-    if (n >= 0 && n <= 15)
-    {
-        for (i = 0; i <= n; i++)
-        {
-            for (j = 0; j <= n; j++)
-            {
-                result = i * j;
+	if (n >= 0 && n <= 15)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				result = i * j;
 
-                if (j == 0)
-                {
-                    _putchar('0' + result);
-                }
-                else
-                {
-                    _putchar(',');
-                    _putchar(' ');
+				if (j == 0)
+				{
+					_putchar('0' + result);
+				}
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
 
-                    if (result < 10)
-                    {
-                        _putchar(' ');
-                        _putchar(' ');
-                        _putchar('0' + result);
-                    }
-                    else if (result >= 10 && result < 100)
-                    {
-                        _putchar(' ');
-                        _putchar('0' + (result / 10));
-                        _putchar('0' + (result % 10));
-                    }
-                    else
-                    {
-                        _putchar('0' + (result / 100));
-                        _putchar('0' + ((result / 10) % 10));
-                        _putchar('0' + (result % 10));
-                    }
-                }
-            }
-            _putchar('\n');
-        }
-    }
+					if (result < 10)
+					{
+						_putchar(' ');
+						_putchar(' ');
+						_putchar('0' + result);
+					}
+					else if (result >= 10 && result < 100)
+					{
+						_putchar(' ');
+						_putchar('0' + (result / 10));
+						_putchar('0' + (result % 10));
+					}
+					else
+					{
+						_putchar('0' + (result / 100));
+						_putchar('0' + ((result / 10) % 10));
+						_putchar('0' + (result % 10));
+					}
+				}
+			}
+			_putchar('\n');
+		}
+	}
 }
 
