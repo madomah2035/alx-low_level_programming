8e8348bd40da55e09ee8b672656ab038b495f6d8
madomah2035 <kwamemusah51@gmail.com>
2023-09-13 22:52:50 -0700

    making changes

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,40 +1,49 @@
 #include "main.h"
 
 /**
- * print_times_table - Print the n times table starting with 0.
- * @n: The value specifying the size of the times table (up to 15).
+ * print_times_table - Print the `n` times table, starting with 0.
+ * Description: If `n` is greater than 15 or less than 0, print nothing.
+ * @n: int type number
  */
 void print_times_table(int n)
 {
-	if (n < 0 || n > 15)
+	int x = 0, y, z;
+
+	if (n > 15 || n < 0)
 		return;
-
-	int i, j;
-
-	for (i = 0; i <= n; i++)
+	while (x <= n)
 	{
-		for (j = 0; j <= n; j++)
+		for (y = 0; y <= n; y++)
 		{
-			int result = i * j;
-
-			if (j == 0)
-				_putchar('0' + result);
+			z = x * y;
+			if (z > 99)
+			{
+				_putchar(z / 100 + '0');
+				_putchar((z / 10 % 10) + '0');
+				_putchar(z % 10 + '0');
+			}
+			else if (z > 9)
+			{
+				_putchar(' ');
+				_putchar(z / 10 + '0');
+				_putchar(z % 10 + '0');
+			}
+			else if (y != 0)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(z + '0');
+			}
 			else
+				_putchar(z + '0');
+
+			if (y != n)
 			{
 				_putchar(',');
 				_putchar(' ');
-
-				if (result < 10)
-					_putchar(' ');
-
-				if (result < 100)
-					_putchar(' ');
-
-				_putchar('0' + (result / 100));
-				_putchar('0' + ((result / 10) % 10));
-				_putchar('0' + (result % 10));
 			}
 		}
 		_putchar('\n');
+		x++;
 	}
 }
--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,25 +1,27 @@
+#include "main.h"
 #include <stdio.h>
+#include <math.h>
 
 /**
- * main - Entry point of the program
- *
- * Return: Always 0 (Success)
+ * main - main block
+ * Description: Print the sum of all multiples of 3 or 5 below 1024.
+ * Return: 0
  */
 int main(void)
 {
-    int sum = 0;
-    int i;
+	int i = 1;
+	int total = 0;
 
-    for (i = 0; i < 1024; i++)
-    {
-        if (i % 3 == 0 || i % 5 == 0)
-        {
-            sum += i;
-        }
-    }
+	while (i < 1024)
+	{
+		if (i % 3 == 0)
+			total += i;
+		else if (i % 5 == 0)
+			total += i;
 
-    printf("%d\n", sum);
+		i++;
+	}
+	printf("%d\n", total);
 
-    return (0);
+	return (0);
 }
-
