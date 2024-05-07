ad260a64d3fdee66c79cbe938a8f51037b16d5f1
madomah2035 <kwamemusah51@gmail.com>
2024-05-07 01:03:26 -0700

    This is ALX search Algorithms

--- 0x1E-search_algorithms/1-main.c
+++ 0x1E-search_algorithms/1-main.c
@@ -18,3 +18,4 @@ int main(void)
     printf("Found %d at index: %d\n\n", 5, binary_search(array, 5, 5));
     printf("Found %d at index: %d\n", 999, binary_search(array, size, 999));
     return (EXIT_SUCCESS);
+}
